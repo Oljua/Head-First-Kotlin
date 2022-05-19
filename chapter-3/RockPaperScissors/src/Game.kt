44e77a546a9a1773498a2fe7a6715cf4e9f9153c
Oljua <olgaavetrova@gmail.com>
2022-05-19 10:22:50 +0300

    adding a function print esult (output game results)

--- chapter-3/RockPaperScissors/src/Game.kt
+++ chapter-3/RockPaperScissors/src/Game.kt
@@ -2,6 +2,7 @@ fun main() {
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
     val userChoice = getUserChoice(options)
+    printResult(userChoice, gameChoice)
 }
 
 fun getGameChoice(optionsParam: Array<String>) =
@@ -31,4 +32,16 @@ fun getUserChoice(optionsParam: Array<String>): String {
         if (!isValidChoice) println("You must enter a valid choice.")
     }
     return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String) {
+    val result: String
+    // Определить результат
+    if (userChoice == gameChoice) result = "Tie!"
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+            (userChoice == "Paper" && gameChoice == "Rock") ||
+            (userChoice == "Scissors" && gameChoice == "Paper")) result = "You win!"
+    else result = "You lose!"
+    // Вывести результат
+    println("You chose $userChoice. I chose $gameChoice. $result")
 }
