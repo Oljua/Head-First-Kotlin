822ccf0b48a4646225f50c58e4097ceb3db46275
Oljua <olgaavetrova@gmail.com>
2022-05-20 14:57:36 +0300

    change in main function

--- chapter-2/CompilerApp2/src/App.kt
+++ chapter-2/CompilerApp2/src/App.kt
@@ -12,7 +12,7 @@ fun main() {
 // Бок кода B
 fun main() {
     val firemen = arrayOf("Pugh", "Pugh", "Barney McGrew", "Cuthbert", "Dibble", "Grub")
-    var firemanNo = 0;
+    var firemanNo = 0
 
     while (firemanNo < 6) {
         println("Fireman number $firemanNo is ${firemen[firemanNo]}")
